c5ca50f3fc0a603df9eac4e943c8c7a388b94f7b
Nicholas Simard <njsimard@hotmail.ca>
2025-03-02 17:32:31 -0700

    Create index page

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useRouter } from "expo-router";
 
 export default function App() {
+
+    const router = useRouter();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <View>
+            <Text>CPRG-303 Labs:</Text>
+            <StatusBar style="auto" />
+        </View>
+        <View>
+            <TouchableOpacity style={styles.button} onPress={() => router.push("lab_1")}>
+                <Text style={styles.buttonText}>Lab 1</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => router.push("lab_2")}>
+                <Text style={styles.buttonText}>Lab 2</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => router.push("lab_3")}>
+                <Text style={styles.buttonText}>Lab 3</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => router.push("lab_4")}>
+                <Text style={styles.buttonText}>Lab 4</Text>
+            </TouchableOpacity>
+        </View>
     </View>
   );
 }
@@ -17,4 +37,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    margin: 5,
+    borderRadius: 10,
+    },
+    buttonText: {
+    color: 'white',
+    },
 });
